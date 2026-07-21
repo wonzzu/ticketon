5af234e8f1ab3c2aa824d62a87f421c928ab139c
wonzzu <wonzzu@users.noreply.github.com>
2026-07-21 19:21:17 +0900

    feat: 결제일 검색 추가 & 배치 로직 테스트 추가

--- src/main/java/com/ticketing/settlement/controller/SettlementController.java
+++ src/main/java/com/ticketing/settlement/controller/SettlementController.java
@@ -11,9 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -46,10 +49,13 @@ public class SettlementController {
             @RequestParam(required = false) Long paymentId,
             @RequestParam(required = false) Long reservationId,
             @RequestParam(required = false) Long eventId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate paidFrom,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate paidTo,
             @AuthenticationPrincipal CustomUserDetails seller,
             @PageableDefault(size = 20) Pageable pageable) {
 
-        SettlementDetailSearchCond cond = new SettlementDetailSearchCond(paymentId, reservationId, eventId);
+        SettlementDetailSearchCond cond =
+                new SettlementDetailSearchCond(paymentId, reservationId, eventId, paidFrom, paidTo);
 
         return ResponseEntity.ok(BaseResponse.success(
                 settlementService.searchMyDetails(seller.getMemberId(), cond, pageable)));
